3540b692324517c350d2ba655a99eeda6e5b8b48
Ghaizan17 <ghaizan.pratama.maulana@gmail.com>
2025-07-18 11:24:10 +0800

    v.1.1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('bg-white');
+    } else {
+        navbar.classList.remove('bg-white');
+    }
+});
+
+
